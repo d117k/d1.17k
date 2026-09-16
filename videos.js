1033504dc9ac92015d9fdd7a272e00f0b35a165a
deeonecinhe-sys <deeonecinhe@gmail.com>
2026-09-16 09:45:11 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,8 +1,14 @@
-// Add your hosted video files here.
-// Example:
-// { title: "Artist — Track", category: "VFX / Music Video", src: "https://your-video-host.com/video.mp4", poster: "" }
-
 const VIDEOS = [
-  // { title: "PROJECT 01", category: "VFX / Music Video", src: "YOUR_VIDEO_URL.mp4", poster: "" },
-  // { title: "PROJECT 02", category: "VFX / Motion", src: "YOUR_VIDEO_URL.mp4", poster: "" },
+  {
+    title: "Faneto",
+    category: "VFX / Music Video",
+    src: "videos/shitted.mp4",
+    poster: "videos/shitted.png"
+  },
+  {
+    title: "raq youngin 3d",
+    category: "VFX / Music Video",
+    src: "videos/notevenaremake.mp4",
+    poster: "videos/notevenaremake.png"
+  }
 ];
