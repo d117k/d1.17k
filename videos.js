4b091d35c531b82b17e17bed47f6c70bc19657ec
d1.17k <deeonecinhe@gmail.com>
2026-09-16 10:22:18 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -3,12 +3,12 @@ const VIDEOS = [
     title: "Faneto",
     category: "VFX / Music Video",
     src: "videos/shitted.mp4",
-    poster: "videos/fanet"
+    poster: "videos/fanet.png"
   },
   {
     title: "raq youngin 3d",
     category: "VFX / Music Video",
     src: "videos/notevenaremake.mp4",
-    poster: "videos/youngin"
+    poster: "videos/youngin.png"
   }
 ];
